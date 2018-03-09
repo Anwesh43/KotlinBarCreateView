90e058a29420e5381d83b5a1ccff83857ed96055
Anwesh43 <anweshthecool0@gmail.com>
2018-03-10 04:02:34 +0530

    adding barcreateview class

--- barcreateview/src/main/java/ui/anwesome/com/barcreateview/BarCreateView.kt
+++ barcreateview/src/main/java/ui/anwesome/com/barcreateview/BarCreateView.kt
@@ -3,3 +3,15 @@ package ui.anwesome.com.barcreateview
 /**
  * Created by anweshmishra on 10/03/18.
  */
+import android.graphics.*
+import android.content.*
+import android.view.*
+class BarCreateView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        return true
+    }
+    override fun onDraw(canvas : Canvas) {
+
+    }
+}
