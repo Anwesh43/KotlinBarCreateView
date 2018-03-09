7ab08f8bb195f9b38a6368e8722cfa692f7ed956
Anwesh43 <anweshthecool0@gmail.com>
2018-03-10 04:12:05 +0530

    adding Animator class

--- barcreateview/src/main/java/ui/anwesome/com/barcreateview/BarCreateView.kt
+++ barcreateview/src/main/java/ui/anwesome/com/barcreateview/BarCreateView.kt
@@ -15,7 +15,7 @@ class BarCreateView(ctx : Context) : View(ctx) {
 
     }
     data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0, var jDir : Int = 1) {
-        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f)
         fun update(stopcb : (Float) -> Unit) {
             scales[j] += 0.1f * dir
             if (Math.abs(scales[j] - prevScale) > 1) {
@@ -34,6 +34,31 @@ class BarCreateView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1 - 2 * prevScale
                 startcb()
+            }
+        }
+    }
+    data class Animator(var view : View, var animated : Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            if (animated) {
+                try {
+                    updatecb()
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch (ex : Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
