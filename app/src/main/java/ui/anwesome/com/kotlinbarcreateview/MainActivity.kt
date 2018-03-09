8c38159d89564d8e1afe24cc69e75bc9f15af375
Anwesh43 <anweshthecool0@gmail.com>
2018-03-10 04:32:29 +0530

    adding View in App

--- app/src/main/java/ui/anwesome/com/kotlinbarcreateview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbarcreateview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinbarcreateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.barcreateview.BarCreateView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BarCreateView.create(this)
     }
 }
